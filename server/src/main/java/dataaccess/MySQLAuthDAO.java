043fc1896acff09f71056dc1f64ae6457ba298f5
John Olson <johnnephiolson@gmail.com>
2024-11-09 14:43:43 -0700

    added all code to MySQL dataaccess methods, based on petshop examples. Need to polish up

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -5,21 +5,39 @@ import model.AuthData;
 public class MySQLAuthDAO implements AuthDAO {
     @Override
     public void clear() {
-
+        var statement = "TRUNCATE auth";
+        executeUpdate(statement);
     }
 
     @Override
     public String createAuth(String authToken, AuthData authData) {
-        return "";
+        var statement = "INSERT INTO auth (name, type, json) VALUES (?, ?, ?)";
+        var json = new Gson().toJson(authData);
+        var id = executeUpdate(statement, authToken, authData.username(), json);
+        return authToken;
     }
 
     @Override
     public AuthData getAuth(String authToken) {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT id, json FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, id);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readPet(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
     public void deleteAuth(String authToken) {
-
+        var statement = "DELETE FROM auth WHERE authToken=?";
+        executeUpdate(statement, authToken);
     }
 }
--- server/src/main/java/dataaccess/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -8,16 +8,33 @@ import java.util.List;
 public class MySQLGameDAO implements GameDAO {
     @Override
     public void clear() {
-
+        var statement = "TRUNCATE pet";
+        executeUpdate(statement);
     }
 
     @Override
     public int createGame(GameData gameData) {
-        return 0;
+        var statement = "INSERT INTO games (gameID, game, json) VALUES (?, ?, ?)";
+        var json = new Gson().toJson(gameData);
+        var id = executeUpdate(statement, gameData.gameID(), gameData, json);
+        return gameData.gameID();
     }
 
     @Override
     public GameData getGame(int gameID) {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameId, json FROM games WHERE id=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, id);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readPet(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
@@ -28,6 +45,20 @@ public class MySQLGameDAO implements GameDAO {
 
     @Override
     public Collection<GameData> listGames() {
-        return List.of();
+        var result = new Collection<GameData>() {
+        };
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameID, json FROM games";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        result.add(readPet(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
     }
 }
