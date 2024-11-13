57c2343c4138f3c324a7229ff764394118b68a71
John Olson <johnnephiolson@gmail.com>
2024-11-12 20:21:35 -0700

    Filled in code for all HTTP methods

--- client/src/main/java/network/HTTPCommunicator.java
+++ client/src/main/java/network/HTTPCommunicator.java
@@ -3,6 +3,7 @@ package network;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.*;
 
 public class HTTPCommunicator {
@@ -11,17 +12,20 @@ public class HTTPCommunicator {
         try {
             URI uri = new URI("http://localhost:8080" + urlPath);
             HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+            http.setReadTimeout(5000);
+            http.setDoOutput(true);
             return http;
         } catch (URISyntaxException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String get(String authToken, String urlPath) {
+    public InputStreamReader get(String authToken, String urlPath) {
         //create url
         HttpURLConnection http = getURLConnection(urlPath);
         try {
             http.setRequestMethod("GET");
+
 
             //create header
             http.addRequestProperty("authorization", authToken);
@@ -31,39 +35,83 @@ public class HTTPCommunicator {
 
             //get and return json response
             try (InputStream respBody = http.getInputStream()){
-                InputStreamReader inputStreamReader = new InputStreamReader(respBody);
-                System.out.println(new Gson().fromJson(inputStreamReader, Map.class));
-
+                return new InputStreamReader(respBody);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return "";
     }
 
-    public String put(String authToken, String jsonBody, String urlPath) {
+    public InputStreamReader put(String authToken, String jsonBody, String urlPath) {
         //create url
-        //create header
-        //create body
-        //send
-        //get and return json response
-        return "";
+        HttpURLConnection http = getURLConnection(urlPath);
+        try {
+            http.setRequestMethod("PUT");
+
+            //create header
+            http.addRequestProperty("authorization", authToken);
+
+            //create body
+            try (OutputStream requestBody = http.getOutputStream();) {
+                requestBody.write(jsonBody.getBytes());
+            }
+
+            //send
+            http.connect();
+
+            //get and return json response
+            try (InputStream respBody = http.getInputStream()){
+                return new InputStreamReader(respBody);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public String delete(String authToken, String urlPath) {
+    public InputStreamReader delete(String authToken, String urlPath) {
         //create url
-        //create header
-        //send
-        //get and return json response\
-        return "";
+        HttpURLConnection http = getURLConnection(urlPath);
+        try {
+            http.setRequestMethod("DELETE");
+
+            //create header
+            http.addRequestProperty("authorization", authToken);
+
+            //send
+            http.connect();
+
+            //get and return json response
+            try (InputStream respBody = http.getInputStream()){
+                return new InputStreamReader(respBody);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public String post(String authToken, String jsonBody, String urlPath) {
+    public InputStreamReader post(String authToken, String jsonBody, String urlPath) {
         //create url
-        //create header
-        //create body
-        //send
-        //get and return json response
-        return "";
+        HttpURLConnection http = getURLConnection(urlPath);
+        try {
+            http.setRequestMethod("POST");
+
+            //create header
+            http.addRequestProperty("authorization", authToken);
+
+            //create body
+            try (OutputStream reqBody = http.getOutputStream();){
+                reqBody.write(jsonBody.getBytes());
+            }
+
+            //send
+            http.connect();
+
+            //get and return json response
+            try (InputStream respBody = http.getInputStream()){
+                return new InputStreamReader(respBody);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- client/src/main/java/network/ServerFacade.java
+++ client/src/main/java/network/ServerFacade.java
@@ -4,34 +4,37 @@ import com.google.gson.Gson;
 import model.UserData;
 import model.requestresult.*;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ServerFacade {
     private HTTPCommunicator httpCommunicator = new HTTPCommunicator();
 
 
 
     public RegisterResponse register(UserData userData) {
-        String json = httpCommunicator.post(null, new Gson().toJson(userData), "/user");
+        InputStreamReader json = httpCommunicator.post(null, new Gson().toJson(userData), "/user");
         return new Gson().fromJson(json, RegisterResponse.class);
     }
 
     public LoginResponse login(LoginRequest request) {
-        String json = httpCommunicator.post(null, new Gson().toJson(request), "/session");
+        InputStreamReader json = httpCommunicator.post(null, new Gson().toJson(request), "/session");
         return new Gson().fromJson(json, LoginResponse.class);
 
     }
 
     public CreateGameResponse createGame(CreateGameRequest request){
-        String json = httpCommunicator.post(request.authToken(), new Gson().toJson(request), "/game");
+        InputStreamReader json = httpCommunicator.post(request.authToken(), new Gson().toJson(request), "/game");
         return new Gson().fromJson(json, CreateGameResponse.class);
     }
 
     public ListGamesResponse listGames(ListGamesRequest request) {
-        String json = httpCommunicator.get(request.authToken(), "/game");
+        InputStreamReader json = httpCommunicator.get(request.authToken(), "/game");
         return new Gson().fromJson(json, ListGamesResponse.class);
     }
 
     public JoinGameResponse joinGame(JoinGameRequest request) {
-        String json = httpCommunicator.put(request.authToken(), new Gson().toJson(request), "/game");
+        InputStreamReader json = httpCommunicator.put(request.authToken(), new Gson().toJson(request), "/game");
         return new Gson().fromJson(json, JoinGameResponse.class);
     }
 
@@ -39,7 +42,7 @@ public class ServerFacade {
     }
 
     public LogoutResponse logout(String authToken) {
-        String json = httpCommunicator.delete(authToken, "/session");
+        InputStreamReader json = httpCommunicator.delete(authToken, "/session");
         return new Gson().fromJson(json, LogoutResponse.class);
     }
 }
