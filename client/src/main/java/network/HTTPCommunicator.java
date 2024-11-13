e1baba63bad5e2e7933c550ce22c6f94a5804468
John Olson <johnnephiolson@gmail.com>
2024-11-12 19:00:12 -0700

    Created functions for GET, PUT, REQUEST, and DELETE in HTTP Communicator class

--- client/src/main/java/network/HTTPCommunicator.java
+++ client/src/main/java/network/HTTPCommunicator.java
@@ -2,19 +2,33 @@ package network;
 
 public class HTTPCommunicator {
 
-    public void get(String auth) {
-
+    public void get(String authToken, String urlPath) {
+        //create url
+        //create header
+        //send
+        //get and return json response
     }
 
-    public void put() {
-
+    public void put(String authToken, String jsonBody, String urlPath) {
+        //create url
+        //create header
+        //create body
+        //send
+        //get and return json response
     }
 
-    public void delete() {
-
+    public void delete(String authToken, String urlPath) {
+        //create url
+        //create header
+        //send
+        //get and return json response
     }
 
-    public void post() {
-
+    public void post(String authToken, String jsonBody, String urlPath) {
+        //create url
+        //create header
+        //create body
+        //send
+        //get and return json response
     }
 }
